7cf22d8300b1d8edb28f117f9abcb54fb37933be
Son of Jongkyun, Park <100128652+jhp2014@users.noreply.github.com>
2023-11-05 16:15:34 +0900

    feat: DarkMode Context 설정
    하위 컴포넌트에서 사용하는 기술을 모르도록 useDarkMode 함수를 export 했다.

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from 'react';
+
+const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+	const [DarkMode, setDarkMode] = useState(false);
+	const toggleDarkMode = () => setDarkMode((prev) => !prev);
+	return (
+		<DarkModeContext.Provider value={(DarkMode, toggleDarkMode)}>
+			{children}
+		</DarkModeContext.Provider>
+	);
+}
+
+//캡슐화 → 다른 컴포넌트에서 사용하는 기술을 모르도록 한다.
+export const useDarkMode = () => useContext(DarkModeContext);
