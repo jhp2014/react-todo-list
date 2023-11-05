3cc8eef9872828a65d4672c0fd0336cd4f8467ec
Son of Jongkyun, Park <100128652+jhp2014@users.noreply.github.com>
2023-11-05 15:40:52 +0900

    feat: Todo filter 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList/TodoList';
+import { useState } from 'react';
+import Header from './Header/Header';
 
+const filters = ['all', 'active', 'completed'];
 function App() {
+	const [filter, setFilter] = useState(filters[0]);
 	return (
 		<div className='App'>
-			<TodoList />
+			<Header filters={filters} filter={filter} onFilterChange={setFilter} />
+			<TodoList filter={filter} />
 		</div>
 	);
 }
